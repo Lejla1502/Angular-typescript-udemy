6cf880ec2d7ad31d483daa88a253cb329367ac09
Lejla1502 <lejlasphotmail.com>
2021-12-01 15:53:10 +0100

    Different types of selectors

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
+ // selector: 'app-servers',
+ // selector:'[app-servers]',
+ selector:'.app-servers', 
+ templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
